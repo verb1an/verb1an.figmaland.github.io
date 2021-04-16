b62513c29217ad4dde8245b886e90e0f284535c7
Verb1an <tabyshkinalex@gmail.com>
2021-04-16 19:07:29 +0700

    F_PC

--- #src/js/index.js
+++ #src/js/index.js
@@ -4,13 +4,14 @@ document.addEventListener('DOMContentLoaded', function()  {
 })
 
 const headerFunctions = () => {
+    const introPT = Number.parseInt(window.getComputedStyle(document.querySelector('#intro')).paddingTop.replace('px', ''));
     window.addEventListener('scroll', function() {
         background();
     });
 
     function background() {
         let scrollHeight = window.pageYOffset;
-        if(scrollHeight >= 130) {
+        if(scrollHeight >= introPT-20) {
             document.querySelector('.header').classList.add('scrolled');
         }else{
             document.querySelector('.header').classList.remove('scrolled');
@@ -21,13 +22,28 @@ const headerFunctions = () => {
 const mobMenu = () => {
     const header = document.querySelector('.header');
     const bar = document.querySelector('.bar');
+    const menuItems = document.querySelectorAll('.nav .nav__item');
+
     document.querySelector('.bar').addEventListener('click', function() {
         let cond = header.classList.contains('active-mobile');
-        
+        toggle(cond);
+    })
+
+    if(window.innerWidth < 769) {
+        menuItems.forEach((el) => {
+            el.addEventListener('click', function() {
+                toggle(true)
+            })
+        })
+    }
+
+    function toggle(cond) {
         if(!cond) {
             header.classList.add('active-mobile');
+            bar.classList.add('active-mobile');
         }else{
             header.classList.remove('active-mobile');
+            bar.classList.remove('active-mobile');
         }
-    })
+    }
 }
